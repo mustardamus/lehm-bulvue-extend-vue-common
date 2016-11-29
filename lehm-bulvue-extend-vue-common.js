30e7f3360138efcb703ae052727fb8942772e9d7
Sebastian Senf <contact@mustardamus.com>
2016-11-29 16:59:18 +0100

    replaced delimiters

--- lehm-bulvue-extend-vue-common.js
+++ lehm-bulvue-extend-vue-common.js
@@ -3,7 +3,7 @@
 module.exports = {
   name: 'Frontend Extend with Vue Common Stuff',
   description: 'https://github.com/mustardamus/lehm-bulvue-extend-vue-common',
-  delimiters: '<% %>',
+  delimiters: '{% %}',
   ignore: ['README.md'],
 
   after: function (srcPath, distPath, variables, utils) {
